309ecf267f9ef61ae9355c4af75a275eecabff23
amir <amirtaki623@gmail.com>
2025-08-10 21:20:47 +0330

    swiper part 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,16 @@ navLink.forEach(n => n.addEventListener("click", linkAction))
 /*=============== SWIPER WACTHES ===============*/
 const swiperWatches = new Swiper (".home__swiper", {
     loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            translate: [-100, 0 ,-500],
+            rotate : [0 , 0, 15],
+            opacity: 0
+        },
+    },
     pagination: {
         el : '.swiper-pagination',
     },
