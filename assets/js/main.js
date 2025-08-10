bc9c32445abcb140c1f0c17853d429f76c2d9a45
amir <amirtaki623@gmail.com>
2025-08-10 21:22:33 +0330

    swiper part 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,9 +37,15 @@ const swiperWatches = new Swiper (".home__swiper", {
             rotate : [0 , 0, 15],
             opacity: 0
         },
+        next: {
+           translate: [100, 0 ,-500],
+            rotate : [0 , 0, -15],
+            opacity: 0
+        },
     },
     pagination: {
         el : '.swiper-pagination',
+        clickable: true,
     },
 })
 
